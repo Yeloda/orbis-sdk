c0437f8f6f9a7aa20993fb8a28b8f2c3b1754c60
Yeloda <86018496+Yeloda@users.noreply.github.com>
2025-11-14 16:16:10 +0100

    Create random-polyfill.ts

---
+++ utils/random-polyfill.ts
@@ -0,0 +1,16 @@
+export function randomBytes(length: number) {
+  const array = new Uint8Array(length);
+  for (let i = 0; i < length; i++) {
+    array[i] = Math.floor(Math.random() * 256);
+  }
+  return array;
+}
+
+export function randomString(length: number) {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return result;
+}
